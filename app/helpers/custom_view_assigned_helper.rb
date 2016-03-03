f0372dbca8ed2d2a82c4e84539cea2439d5a2436
Frederico Camara <frederico.camara@serpro.gov.br>
2016-03-03 17:56:29 -0300

    Fix: Calculates according to old_issues of user roles

--- app/helpers/custom_view_assigned_helper.rb
+++ app/helpers/custom_view_assigned_helper.rb
@@ -2,7 +2,7 @@ module CustomViewAssignedHelper
   def assignable_users(issue)
     current_project = Project.find(issue.project_id)
 
-    workflow_rules = WorkflowRule.where('new_status_id = ?', issue.status_id).group(:role_id).pluck(:role_id)
+    workflow_rules = WorkflowRule.where('old_status_id = ? AND tracker_id = ? AND type = ?', issue.status_id, issue.tracker_id, WorkflowTransition).group(:role_id).pluck(:role_id)
     target_roles = MemberRole.select { |role| workflow_rules.include?(role.role_id) }.map(&:member_id).sort
     target_members = Member.select { |member| target_roles.include?(member.id) &&
         member.project_id == current_project.id }.map(&:user_id).sort
@@ -12,13 +12,6 @@ module CustomViewAssignedHelper
 
     users = current_project.member_principals.select { |m| types.include?(m.principal.type) &&
         m.roles.detect(&:assignable) && target_members.include?(m.principal.id) }.map(&:principal).sort
-
-    author = User.find(issue.author_id)
-    assigned_to = User.find(issue.assigned_to_id) if issue.assigned_to_id
-
-    users << author if author
-    users << assigned_to if assigned_to
-
     users.uniq.sort
   end
 end
